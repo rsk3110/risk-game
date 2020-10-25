c862c69d6cee2354e0f7ceb7324d6b1c5dde67b8
Kaue Oliveira <elmovckaue@gmail.com>
2020-10-25 03:25:49 -0400

    Comment MapCommand

--- src/main/java/io/github/rsk3110/riskgame/MapCommand.java
+++ src/main/java/io/github/rsk3110/riskgame/MapCommand.java
@@ -2,11 +2,26 @@ package io.github.rsk3110.riskgame;
 
 import java.util.List;
 
+/**
+ * Lists a map of the World in format:
+ * ContinentName (Bonus Armies: #) {
+ *     TerrytoryName: Occupied by PlayerName with # armies.
+ *     ...
+ * }
+ *
+ * @author Kaue Gomes e Sousa de Oliveira
+ */
 public class MapCommand implements Command {
 
     public MapCommand(){
     }
 
+    /**
+     * Simply outputs continent.toString() for all continents, recursively executing territory.toString().
+     *
+     * @param player player executing the command
+     * @return whether to hand control to next player
+     */
     public boolean execute(Player player) {
         for(Continent continent : player.getWorld().getContinents()) {
             System.out.println(continent);
@@ -14,7 +29,15 @@ public class MapCommand implements Command {
         return false;
     }
 
+    /**
+     * Map cannot be executed with any arguments.
+     *
+     * @param player player executing the command
+     * @param args arguments of execution
+     * @return whether to hand control to next player
+     */
     public boolean execute(Player player, List<String> args) {
+        System.out.println("Invalid arguments. Quit does not take in any arguments. {map}");
         return false;
     }
 }
