6d5ade5fb3fdcd8f5056e5bb291d060122762edf
ToobaKS <tksheikh17@gmail.com>
2020-10-25 14:18:10 -0400

    Added documentation classes

--- src/main/java/io/github/rsk3110/riskgame/TerritoryEdge.java
+++ src/main/java/io/github/rsk3110/riskgame/TerritoryEdge.java
@@ -3,7 +3,7 @@ package io.github.rsk3110.riskgame;
 import org.jgrapht.graph.DefaultEdge;
 
 /**
- *
+ * Connects different territories, helping set which territory can be reached from where
  *
  * @author Mark Johnson
  **/
