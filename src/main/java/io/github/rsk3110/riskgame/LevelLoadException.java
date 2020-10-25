25f8c13f40ae4224c684b24b1f7d258e5bd5f941
Kaue Oliveira <elmovckaue@gmail.com>
2020-10-25 17:42:53 -0400

    Review LevelLoadException docs

--- src/main/java/io/github/rsk3110/riskgame/LevelLoadException.java
+++ src/main/java/io/github/rsk3110/riskgame/LevelLoadException.java
@@ -1,5 +1,10 @@
 package io.github.rsk3110.riskgame;
 
+/**
+ * Generic exception for level loading.
+ *
+ * @author Mark Johnson
+ */
 public class LevelLoadException extends RuntimeException {
 
     public LevelLoadException(String message) {
