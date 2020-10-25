a4cc00ba605dde09283e289751f03b2cc3bbe5d2
Kaue Oliveira <elmovckaue@gmail.com>
2020-10-25 00:38:55 -0400

    Add territoryMap to World

--- src/main/java/io/github/rsk3110/riskgame/World.java
+++ src/main/java/io/github/rsk3110/riskgame/World.java
@@ -3,22 +3,35 @@ package io.github.rsk3110.riskgame;
 import org.jgrapht.Graph;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public final class World implements Serializable {
     private final Graph<Territory, TerritoryEdge> graph;
     private final List<Continent> continents;
+    private final Map<Territory, Set<TerritoryEdge>> territoryMap;
 
     public World(final Graph<Territory, TerritoryEdge> graph, final List<Continent> continents) {
         this.graph = graph;
         this.continents = continents;
+        this.territoryMap = new HashMap<Territory, Set<TerritoryEdge>>() {{
+            for(Territory territory : graph.vertexSet()) {
+                put(territory, graph.edgesOf(territory));
+            }
+        }};
     }
 
     public Graph<Territory, TerritoryEdge> getGraph() {
-        return this.graph;
+        return graph;
     }
 
     public List<Continent> getContinents() {
-        return this.continents;
+        return continents;
+    }
+
+    public Map<Territory, Set<TerritoryEdge>> getTerritoryMap() {
+        return territoryMap;
     }
 }
