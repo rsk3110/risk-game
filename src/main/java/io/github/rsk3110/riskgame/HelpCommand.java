29141d9f2a4c18efb6ec8f0d9969911dc3184530
Kaue Oliveira <elmovckaue@gmail.com>
2020-10-25 02:52:52 -0400

    Organize HelpCommand

--- src/main/java/io/github/rsk3110/riskgame/HelpCommand.java
+++ src/main/java/io/github/rsk3110/riskgame/HelpCommand.java
@@ -1,49 +1,64 @@
-
 package io.github.rsk3110.riskgame;
 
 import java.util.List;
-/*
-* Tells player how to play the game
-* Explains game set up.
-* All commands and their descriptions
-* Rules for attacking and winning.
-*
-* */
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Tells player how to play the game
+ * Explains game set up.
+ * All commands and their descriptions
+ * Rules for attacking and winning.
+ * @author Tooba Sheikh
+ * @author Kaue Gomes e Sousa de Oliveira
+ **/
 public class HelpCommand implements Command {
 
+    private Map<String, String> shortMessages;
+    private Map<String, String> longMessages;
+
     public HelpCommand(){
+        this.shortMessages = new TreeMap<String, String>(){{
+            put("help", "help | help <command> - tells you how to play the game. Optionally explains command if specified.");
+            put("map", "map | map <continent> - Shows all continents and territories statuses. Optionally only shows territories of specified continent.");
+            put("attack", "attack <origin> <target> - Attacks target territory from origin territory. See 'help attack' for more.");
+            put("skip", "skip - skips your turn and goes to the next player.");
+            put("quit", "quit - quits the program.");
+        }};
 
+        this.longMessages = new TreeMap<String, String>(){{
+            put("help", shortMessages.get("help"));
+            put("map", shortMessages.get("map"));
+            put("attack", "attack <origin> <target> - Attacks target territory from origin territory.\n" +
+                    "Attacker chooses 1 - 3 dice, requiring the number of dice + 1 armies at origin to select.\n" +
+                    "Defender choose 1 - 2 dice, requiring the number of dice + 1 armies at target to select.\n" +
+                    "Highest die are compared until territory is lost (target army hits 0), or a player uses up all their dice.\n" +
+                    "If attack wins comparison, target loses 1 army. If defender wins or ties, origin loses 1 army.");
+            put("skip", shortMessages.get("skip"));
+            put("quit", shortMessages.get("quit"));
+        }};
     }
 
-    public static void help(){
-        //Game intro
-        System.out.println("You are playing Risk.");
-        System.out.println(
-                "There are 6 continents (no Antarctica) and 42 territories divided among them./n " +
-                        "There are two players (in this version) each with 21 territories./n" +
-                        "Each of the territories have 2 - 3 armies that add up to  a total of 50 armies per player./n");
-
-        //Game commands
-        System.out.println("There 5 commands that you can use: ");
-        System.out.println("1. Map: shows the status of the world, which territory belong to which player and the number of armies.");
-        System.out.println("2. Help: tells you how to play the game.");
-        System.out.println("3. Skip: Skips your turn and goes to the next player.");
-        System.out.println("4. Quit: Quits the program");
-        System.out.println("5. Attack: Start by selecting your own territory. Then three different modes of play: /n" +
-                " - 'Attack' a your own bordering territory - will result in moving armies from first territory to second./n" +
-                " - Attack a neutral territory - will move armies from your territory to a neutral (unowned) territory./n" +
-                " - Attack an opponent: You choose how many dice to roll and who ever has the smaller dice will lose one army./n");
-
-        //Win conditions
-        System.out.println("The that takes over the world is the winner.");
-        System.out.println("Otherwise its a draw.");
-    }
     public boolean execute(Player player) {
-        help();
+        for(String cmd : shortMessages.keySet())
+            System.out.println(shortMessages.get(cmd));
+
         return false;
     }
 
     public boolean execute(Player player, List<String> args) {
+        if(args.size() != 1) {
+            System.out.println("Invalid number of arguments. {help | help <command>}");
+            return false;
+        }
+
+        String cmd = args.get(0);
+        if(!longMessages.containsKey(cmd)) {
+            System.out.println("Command '" + cmd + "'  does not exist. {help | help <command>}");
+            return false;
+        }
+
+        System.out.println(longMessages.get(cmd));
         return false;
     }
 }
